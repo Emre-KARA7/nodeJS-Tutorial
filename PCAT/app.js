4f989918816ceea0440ddebcd0a232354f1a99b1
Emre Kara <emre.kara11@ogr.sakarya.edu.tr>
2023-03-12 04:44:50 +0300

    express nodemon initialized, static folder created

--- PCAT/app.js
+++ PCAT/app.js
@@ -1,3 +1,15 @@
-const fn = function () {
-  console.log('function');
-};
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//Middlewares
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`sunucu ${port} nu lu portta calidsiyor`);
+});
